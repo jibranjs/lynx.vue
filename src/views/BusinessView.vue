1655467cd2a2e99a96ded691ffa91ed6ce513fec
jibran <jibranshaheer2@gmail.com>
2025-10-28 11:14:52 +0500

    Test file rmeoved.

--- src/views/BusinessView.vue
+++ src/views/BusinessView.vue
@@ -3,11 +3,9 @@
 
     <BestnusinessComponent />
 
-    <DownloadappComponent />
 
 </template>
 <script setup>
 import BestnusinessComponent from '../components/BusinessComponents/BestbusinessComponent.vue'
 import BusinesstaxiComponent from '../components/BusinessComponents/CorporatetaxiComponent.vue'
-import DownloadappComponent from '../components/Homepage/DownloadappComponent.vue';
 </script>
