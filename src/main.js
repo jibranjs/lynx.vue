77ed918e60083c9ddb4c286a08bd020683d2a844
jibran <jibranshaheer2@gmail.com>
2025-10-28 11:13:50 +0500

    File structure changes.

--- src/main.js
+++ src/main.js
@@ -3,17 +3,15 @@ import './assets/css/main.css'
 import App from './App.vue'
 import router from './router'
 
-import PrimeVuePlugin from './plugins/primevue'
+
 
 // Other components
 import { Icon } from '@iconify/vue'
-
 const app = createApp(App)
 
 app.component('Icon', Icon)
 
 app.use(router)
-app.use(PrimeVuePlugin)
 
 // Mount app
 app.mount('#app')
--- src/views/BusinessView.vue
+++ src/views/BusinessView.vue
@@ -1,6 +1,13 @@
 <template>
-<BusinesstaxiComponent />
+    <BusinesstaxiComponent />
+
+    <BestnusinessComponent />
+
+    <DownloadappComponent />
+
 </template>
 <script setup>
+import BestnusinessComponent from '../components/BusinessComponents/BestbusinessComponent.vue'
 import BusinesstaxiComponent from '../components/BusinessComponents/CorporatetaxiComponent.vue'
+import DownloadappComponent from '../components/Homepage/DownloadappComponent.vue';
 </script>
